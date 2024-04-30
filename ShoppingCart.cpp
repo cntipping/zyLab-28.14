da22076713d02072981fbd069e4e3760c0f7274f
cntipping <167351806+cntipping@users.noreply.github.com>
2024-04-30 14:54:40 -0500

    Update ShoppingCart.cpp
    Added function headers. Wrote constructors and accessors (GetCustomerName(), GetDate()).

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -1,1 +1,46 @@
 /* Type your code here */
+#include <iostream>
+using namespace std;
+
+#include "ShoppingCart.h"
+
+ShoppingCart::ShoppingCart(){
+    customerName = "none";
+    currentDate = "January 1, 2016";
+}
+ShoppingCart::ShoppingCart(string name, string date){
+    customerName = name;
+    currentDate = date;
+}
+      
+string ShoppingCart::GetCustomerName() const{
+    return customerName;
+}
+string ShoppingCart::GetDate() const{
+    return currentDate;
+}
+
+void ShoppingCart::AddItem(ItemToPurchase item){
+    
+}
+void ShoppingCart::RemoveItem(string name){
+
+}
+      
+void ShoppingCart::ModifyItem(ItemToPurchase item){
+
+}
+      
+int ShoppingCart::GetNumItemsInCart(){
+
+}
+double ShoppingCart::GetCostOfCart(){
+
+}
+      
+void ShoppingCart::PrintTotal(){
+
+}
+void ShoppingCart::PrintDescriptions(){
+
+}
