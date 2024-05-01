7134886fbaa68f963b340caea770c73a5cf03db8
mollieeh <167226305+mollieeh@users.noreply.github.com>
2024-04-30 21:10:26 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,16 +6,40 @@ using namespace std;
 
 void PrintMenu() {
    /* Type your code here */
-   
+   cout << "MENU" << endl
+   << "a - Add item to cart" << endl
+   << "d - Remove item from cart" << endl
+   << "c - Change item quantity" << endl
+   << "i - Output items' descriptions" << endl
+   << "o - Output shopping cart" << endl
+   << "q - Quit" << endl;
 }
 
 void ExecuteMenu(char option, ShoppingCart& theCart) {
    /* Type your code here */
+   if (option == 'a')
+   {
+   }
+   else if (option == 'd')
+   {
+   }
+   else if (option == 'i')
+   {
+   }
+   else if (option == 'o')
+   {
+   }
+   else if (option == 'q')
+   {
+   }
    
 }
 
 int main() {
    /* Type your code here */
+   cout << "Enter customer's name: " << endl;
+
+   cout << "Enter today's date:" << endl;
    
    return 0;
 }
